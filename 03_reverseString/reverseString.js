b6a7756be86149e43969b9a32a6bb94efe0f23da
Nahid Reza <nahidreza99@gmail.com>
2023-01-21 01:51:37 +0600

    Solved frist four exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var newStr = ''
+    for(var i = str.length-1; i >= 0 ; i--){
+        newStr += str[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
